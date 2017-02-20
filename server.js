aceccaeeb4d83c9d1ad27a588f8515ba4bbcd780
Boris <borya.ivchenko@mail.ru>
2017-02-20 10:39:53 +0000

    completed

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 var http = require('http');
 var url = require('url');
 
 var server = http.createServer(function(req, res){
 	var pathname = url.parse(req.url , true).pathname.slice(1);
 	var result;
-	var reg = /^\d{1,13}$/;
+	var regUnix = /^\d{1,13}$/;
+	var regNatural = /^[a-zA-Z]{3,}%\d{1,2},?%\d{4,}$/;
 
 	if(pathname === '')
-		result = 'Timestamp Microservice'
+		result = 'Timestamp Microservice';
 
-	else if(pathname.match(reg) != null){
+	else if(pathname.match(regUnix) != null)
 		result = output(parseInt(pathname), timeConverter(pathname));
-
+		
+	else if(pathname.match(regNatural) != null){
+		var data = pathname.split('%');
+		var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+		var regex = new RegExp(data[0], "i");
+		var month = months.filter(function(item){
+			return item.search(regex) != -1;
+		});
+		var date = month + ' ' + data[1].replace(',', '') + ', ' + data[2];
+		var ms = Math.round(new Date(date).getTime()/1000.0)
+		result = output(parseInt(ms), date);
 	}
 	
 	else
